5568e551e73f552a60b9c6727495d2ea6e744442
zokirovbobur <33698387+zokirovbobur@users.noreply.github.com>
2019-07-27 19:11:53 +0500

    Add files via upload

--- src/main/java/com/colibrisoft/app_jar/RequestService.java
+++ src/main/java/com/colibrisoft/app_jar/RequestService.java
@@ -9,9 +9,17 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 @RestController
 @RequestMapping
-public class RequestService {
+public class RequestService{
+    private static final String token = "906702482:AAHjNeLwWupLXfSO3yj7kOEojzj19ubxGbE";
+
     @Autowired
     private JavaMailSender javaMailSender;
 
@@ -28,12 +36,37 @@ public class RequestService {
 
     @PostMapping("userRequest")
     public ResponseEntity<Object> userRequest(@RequestBody RequestForm requestForm){
-        String content = "username: " + requestForm.getUserName() + "\n" +
+        String contentUrl = "username: " + requestForm.getUserName() + "%0A" +
+                "phone: " + requestForm.getPhone() + "%0A" +
+                "service type: " + requestForm.getServiceType();
+
+        String contentMail = "username: " + requestForm.getUserName() + "\n" +
                 "phone: " + requestForm.getPhone() + "\n" +
                 "service type: " + requestForm.getServiceType();
+
+        sendToTelegram(contentUrl,"243053893");  //BBro private chat id
+        sendToTelegram(contentUrl,"47833754");   //Bosithon private chat id
+
         simpleMail(new RequestMail("colibrisoftofficial@gmail.com",
-                "New request from colibrisoft website",content));
+                "New request from colibrisoft website",contentMail));
         return ResponseEntity.ok("Message has been sent successfully");
+    }
+
+    public static void sendToTelegram(String text,String chatId) {
+        new Thread(()->{
+            String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
+            urlString = String.format(urlString, token, chatId, text);
+            try {
+                URL url = new URL(urlString);
+                URLConnection conn = url.openConnection();
+                InputStream is = new BufferedInputStream(conn.getInputStream());
+            } catch (IOException e) {
+                System.out.println("from: " + chatId);
+                e.getCause().getMessage();
+            }
+            System.out.println("Telegram Thread ended");
+        }).start();
+
     }
 
     private void simpleMail(RequestMail requestMail){
@@ -48,6 +81,8 @@ public class RequestService {
             msg.setSubject(title);
             msg.setText(content);
             javaMailSender.send(msg);
+
+            System.out.println("Mail Thread ended");
         }).start();
     }
     private String api(){
